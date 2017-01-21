2b93c22ec0ce893f4a7cb09b816086a62217a453
Toni Barth <software@satoprogs.de>
2017-01-21 17:02:43 +0100

    inserted balance data

--- towers/water_tower.py
+++ towers/water_tower.py
@@ -7,9 +7,9 @@ class WaterTower(tower.Tower):
 	effect_desc = ("Stuns a couple of enemies in a line for ", "[stun]", "s")
 	def __init__(self):
 		tower.Tower.__init__(self)
-		self.EffectValue=0.3 # in seconds
-		self.EffectType=tower.EFFECT_TYPE_DAMAGE|tower.EFFECT_TYPE_ALL|tower.EFFECT_TYPE_STRAIGHT
-		self.Speed=12
+		self.EffectValue=20 # percentage
+		self.EffectType=tower.EFFECT_TYPE_SLOWDOWN|tower.EFFECT_TYPE_STRAIGHT
+		self.Speed=5
 		self.setSprite('assets/level/towers/watertower.png')
 		self.setPlaceSound("assets/sound/water_tower/place.ogg")
 		self.setAttackSound("assets/sound/water_tower/attack.ogg")
