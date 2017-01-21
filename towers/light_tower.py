3bea68ec846b374b8b72edbde5666ad9b7418ca9
Erik Tauchert <erik@tauchert.de>
2017-01-21 11:19:28 +0100

    fix light tower

--- towers/light_tower.py
+++ towers/light_tower.py
@@ -10,5 +10,5 @@ class LightTower(tower.Tower):
 		self.EffectType=tower.EFFECT_TYPE_SLOWDOWN|tower.EFFECT_TYPE_SINGLE
 		self.EffectValue=10 # percentage
 		self.Speed=8.0 # in seconds
-		self.setSprite('assets/level/towers/lighttower.png'
+		self.setSprite('assets/level/towers/lighttower.png')
 		self.setPlaceSound("assets/sound/light_tower/place.ogg")
