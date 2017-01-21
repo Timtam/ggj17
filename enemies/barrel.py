ae2cfd2b400084e624ab959b8af4b2401959056d
Erik Tauchert <erik@tauchert.de>
2017-01-21 16:18:26 +0100

    change enemy values to balanced values

--- enemies/barrel.py
+++ enemies/barrel.py
@@ -4,10 +4,10 @@ from enemy import Enemy
 class Barrel(Enemy):
 	def __init__(self):
 		super(Barrel,self).__init__()
-		self.health  = 100
-		self.speed   = 0.5
+		self.health  = 75
+		self.speed   = 0.75
 		self.sound   = None
 		self.damage  = 1
 		self.setDieSound('assets/sound/barrelguy/die.ogg')
 		self.setSprite("barrel")
-		self.drop    = randint(1,3)
+		self.drop    = 5 + randint(-2, 2)
--- enemies/skeleton.py
+++ enemies/skeleton.py
@@ -3,11 +3,11 @@ from enemy import Enemy
 
 class Skeleton(Enemy):
 	def __init__(self):
-		super(Skelett,self).__init__()
-		self.health  = 100
-		self.speed   = 0.5
+		super(Skeleton,self).__init__()
+		self.health  = 125
+		self.speed   = 1.25
 		self.sound   = None
 		self.damage  = 1
 		self.setDieSound('assets/sound/skeleton/die.ogg')
 		self.setSprite("skeleton")
-		self.drop    = randint(1,3)
+		self.drop    = 10 + randint(-2, 2)
