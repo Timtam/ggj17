8f96eba1998c15a3bf6e2c7e3026acf10ccfd041
Toni Barth <software@satoprogs.de>
2017-01-22 14:42:16 +0100

    splashscreen enabled

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Main:
 		}
 		self.next_view = None
 		self.current_view = None
-		self.change_view('MainMenu')
+		self.change_view('SplashScreen')
 		#self.current_view = Level(self.screen)
 		pygame.display.set_caption('Wavomizer')
 
