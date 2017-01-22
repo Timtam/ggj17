b176c23bca90e83e47290283c6eff5e8780e6b8f
Toni Barth <software@satoprogs.de>
2017-01-22 12:20:58 +0100

    splash screen will now play our whole splash screen sound splash screen unnecessary silence trimmed

--- splash_screen.py
+++ splash_screen.py
@@ -1,19 +1,20 @@
 import pygame
-import time
 
 from commons import *
 from controls import *
+from Bass4Py.Bass4Py.constants import BASS_ACTIVE_PLAYING
 
 class SplashScreen:
 	def __init__(self, screen):
 		self.screen = screen
 		self.splash = pygame.transform.scale(get_common().get_image('assets/ui/splash-16by9.png'), self.screen.get_size())
-		self.start_time = time.time()
+		self.sound = play_sound_fx("assets/sound/ui/splash_screen.ogg")
 
 	def leave(self): pass
 	def handle_ev(self, event): pass
 	def update(self):
-		if (time.time() - self.start_time) >= 2.5:
-			get_common().get_main().change_view('MainMenu')
+		if self.sound.Active==BASS_ACTIVE_PLAYING:
+			return
+		get_common().get_main().change_view('MainMenu')
 	def render(self):
 		self.screen.blit(self.splash, (0, 0))
