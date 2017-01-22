750b1f139a584095b941eb31b2b8a30e60744d74
Blende8 <moritz.bloch@gmail.com>
2017-01-22 11:35:01 +0100

    Credits

--- towers/water_tower.py
+++ towers/water_tower.py
@@ -7,9 +7,9 @@ class WaterTower(tower.Tower):
 	effect_desc = ("Slows one enemy in a line for ", "[stun]", "%")
 	def __init__(self):
 		tower.Tower.__init__(self)
-		self.EffectValue=60 # percentage
+		self.EffectValue=30 # percentage
 		self.EffectType=tower.EFFECT_TYPE_SLOWDOWN|tower.EFFECT_TYPE_STRAIGHT
-		self.Speed=0.5 #attc speed
+		self.Speed=2 #attc speed
 		self.setSprite('watertower')
 		self.setPlaceSound("assets/sound/water_tower/place.ogg")
 		self.setAttackSound("assets/sound/water_tower/attack.ogg")
