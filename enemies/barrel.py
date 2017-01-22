ba41ad46ba3776a2ae37764fd179a7acfc563138
Blende8 <moritz.bloch@gmail.com>
2017-01-22 05:35:47 +0100

    balance

--- enemies/barrel.py
+++ enemies/barrel.py
@@ -4,8 +4,8 @@ from enemy import Enemy
 class Barrel(Enemy):
 	def __init__(self):
 		super(Barrel,self).__init__()
-		self.health  = self.max_health = 125
-		self.speed   = 0.5
+		self.health  = self.max_health = 100
+		self.speed   = 0.3
 		self.setDieSound('assets/sound/barrelguy/die.ogg')
 		self.setSprite("barrel")
 		self.drop    = 6 + randint(-2, 2)
